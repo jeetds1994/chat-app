aa9aa61caba43c34b47fc97b52aef4178c3b4df0
Diljeet Singh <jeetds1994@gmail.com>
2017-12-02 20:58:05 -0500

    app now stores messages

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require("express")
 
+var bodyParser = require("body-parser")
+
 var app = express()
 
 app.use(express.static(__dirname))
+
+app.use(bodyParser.json())
+
+
+app.use(bodyParser.urlencoded({extended: false}))
 
 var messages = [
     {name: "Eli", message: "Hello World!"},
@@ -11,6 +18,12 @@ var messages = [
 
 app.get("/messages", (req, res) => {
     res.send(messages)
+})
+
+
+app.post("/messages", (req, res) => {
+    messages.push(req.body)
+    res.sendStatus(200)
 })
 
 var server = app.listen(3000, function(){
