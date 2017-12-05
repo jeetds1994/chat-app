3c741c91e662ccc135433e6660e06f81059fb350
Diljeet Singh <jeetds1994@gmail.com>
2017-12-05 12:45:02 -0500

    messages now persist to mongodb through mongoose

--- server.js
+++ server.js
@@ -19,20 +19,28 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 var dbURL = "mongodb://user:1234@ds129946.mlab.com:29946/learning-node"
 
-var messages = [
-    {name: "Eli", message: "Hello World!"},
-    {name: "David", message: "The Avenegers!"}
-]
+var Message = mongoose.model("Message", {
+  name: String,
+  message: String
+})
 
 app.get("/messages", (req, res) => {
-    res.send(messages)
+    Message.find({}, (err, messages) => {
+      res.send(messages)
+    })
 })
 
 
 app.post("/messages", (req, res) => {
-    messages.push(req.body)
-    io.emit("message", req.body)
-    res.sendStatus(200)
+    var message = new Message(req.body)
+
+    message.save((err) => {
+      if(err){
+        sendStatus(500)
+      }
+      io.emit("message", req.body)
+      res.sendStatus(200)
+    })
 })
 
 io.on("connection", function(socket){
